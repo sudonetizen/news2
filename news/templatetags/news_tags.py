c5e40829d7f4be75c1d533223c9a676eb81ac0d2
sudonetizen <sardortolibjonov1039@gmail.com>
2026-04-03 13:04:38 +0500

    add markdown filter for articles

--- news/templatetags/news_tags.py
+++ news/templatetags/news_tags.py
@@ -1,7 +1,15 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.filter(name='has_group')
 def has_group(user, group):
     return user.groups.filter(name=group).exists()
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
+
